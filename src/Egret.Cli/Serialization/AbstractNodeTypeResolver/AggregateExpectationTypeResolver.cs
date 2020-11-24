0e5279141dd5a95c4853cf5b32677b5e1eb15d4e
Anthony Truskinger <anthony.truskinger@gmail.com>
2020-11-24 20:16:14 +1000

    Fix spelling mistake

--- src/Egret.Cli/Serialization/AbstractNodeTypeResolver/AggregateExpectationTypeResolver.cs
+++ src/Egret.Cli/Serialization/AbstractNodeTypeResolver/AggregateExpectationTypeResolver.cs
@@ -52,7 +52,7 @@ namespace Egret.Cli.Serialization
 
         private void FailEmpty()
         {
-            throw new Exception($"Could not determin expectation type, {targetKey} has an empty value");
+            throw new Exception($"Could not determine expectation type, {targetKey} has an empty value");
         }
 
         private Type CheckName(string value)
@@ -62,7 +62,7 @@ namespace Egret.Cli.Serialization
                 return childType;
             }
 
-            var known = (typeLookup.Keys).JoinWithComma();
+            var known = typeLookup.Keys.JoinWithComma();
             throw new Exception($"Could not match `{targetKey}: {value} to a known expectation. Expecting one of: {known}");
         }
     }
